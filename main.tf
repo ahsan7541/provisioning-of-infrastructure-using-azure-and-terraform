3508812a802a71be3bb14414c4338a7710c5eb7b
Ahsan Ali Chaudhary <48394941+ahsan7541@users.noreply.github.com>
2023-07-11 09:07:59 +0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,66 @@
+provider "azurerm" {
+  version = "=2.0.0"
+  features {}
+  subscription_id = "YOUR_SUBSCRIPTION_ID"
+  client_id       = "YOUR_CLIENT_ID"
+  client_secret   = "YOUR_CLIENT_SECRET"
+  tenant_id       = "YOUR_TENANT_ID"
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "my-resource-group"
+  location = "West US"
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = "my-virtual-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = "my-subnet"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_linux_virtual_machine" "example" {
+  name                = "my-vm"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  size                = "Standard_DS1_v2"
+  admin_username      = "myadmin"
+  admin_password      = "P@ssw0rd1234!"
+  disable_password_authentication = false
+
+  network_interface_ids = [azurerm_network_interface.example.id]
+
+  os_disk {
+    name              = "my-vm-osdisk"
+    caching           = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+
+  computer_name = "my-vm"
+}
+
+resource "azurerm_network_interface" "example" {
+  name                = "my-nic"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "my-nic-config"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
